1d5be6a6ce8739efb8dda51f4d4aee325b516fd5
Andrea Berardi <andrea.berardi@verbinteractive.com>
2025-11-06 18:04:03 +0100

    Setup eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,38 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettierConfig from 'eslint-config-prettier/flat';
+import prettierPlugin from 'eslint-plugin-prettier/recommended';
 
 const eslintConfig = defineConfig([
+  // base from Next.js
   ...nextVitals,
   ...nextTs,
+
+  // rules overrides
+  {
+    rules: {
+      'react/display-name': 'off',
+      'prettier/prettier': 'error',
+      'react/no-array-index-key': 'error',
+      curly: ['warn', 'all'],
+      'arrow-body-style': ['warn', 'as-needed'],
+      'no-unused-vars': 'error',
+    },
+  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
   ]),
+
+  // prettier integration
+  prettierConfig,
+  prettierPlugin,
 ]);
 
 export default eslintConfig;
