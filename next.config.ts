89c81e778c9db08bf6b409232c41a8f4cf05e067
Andrea Berardi <andrea.berardi@verbinteractive.com>
2025-11-06 18:07:04 +0100

    Next.js base setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      /*
+      {
+        hostname: process.env.NEXT_IMAGE_DOMAIN ?? '',
+      },
+      */
+      { hostname: 'img.youtube.com' },
+    ],
+    deviceSizes: [375, 640, 768, 1024, 1440, 1920, 2560, 2880, 3840],
+    imageSizes: [64, 128, 192],
+  },
+  poweredByHeader: false,
 };
 
 export default nextConfig;
