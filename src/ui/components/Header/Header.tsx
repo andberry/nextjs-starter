c47259764035ea6352c8a1c00b2c3e7a176e8324
Andrea Berardi <syneus@gmail.com>
2025-11-08 23:26:56 +0100

    Header

--- src/ui/components/Header/Header.tsx
+++ src/ui/components/Header/Header.tsx
@@ -1,11 +1,23 @@
 import { Container } from '@components/helpers/Container';
+import { Rocket } from 'lucide-react';
+import { Twitter, Facebook, Instagram } from 'lucide-react';
 export const Header = () => {
   return (
-    <header className="font-urbanist text-secondary bg-black py-4 text-lg font-normal tracking-wider uppercase">
+    <header className="font-urbanist bg-black py-4 text-lg font-normal tracking-wider text-white uppercase">
       <Container size="3xl">
         <div className="flex justify-between">
-          <div>Header Left</div>
-          <div>Header Right</div>
+          <div className="flex gap-4">
+            <Rocket />
+            Berry Next.js Starter
+          </div>
+          <div className="flex gap-12">
+            <div>Nav Here</div>
+            <div className="flex gap-2">
+              <Twitter strokeWidth={1} />
+              <Facebook strokeWidth={1} />
+              <Instagram strokeWidth={1} />
+            </div>
+          </div>
         </div>
       </Container>
     </header>
