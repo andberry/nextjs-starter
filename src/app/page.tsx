191783708b2816f00aada0756a60a8a72c1d5604
Andrea Berardi <syneus@gmail.com>
2025-11-10 00:08:10 +0100

    homepage cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,10 @@
-import Image from 'next/image';
+import { Copy } from '@/ui/components/Typography/Copy/Copy';
 
 export default function Home() {
   return (
     <div className="bg-white font-sans">
       <main>
-        <Image
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim dolore,
-          architecto optio repellat recusandae exercitationem ratione libero
-          temporibus ducimus fugit non, blanditiis illum hic, placeat cupiditate
-          laudantium voluptatum deserunt eos.
-        </div>
+        <Copy size="large">This is homepage</Copy>
       </main>
     </div>
   );
