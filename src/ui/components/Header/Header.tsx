37865658f1cba8243451722fe4ce98ac23a8c932
Andrea Berardi <syneus@gmail.com>
2025-11-09 01:06:57 +0100

    Adding IconLink base component

--- src/ui/components/Header/Header.tsx
+++ src/ui/components/Header/Header.tsx
@@ -3,6 +3,7 @@ import { FaFacebook } from 'react-icons/fa';
 import { FaTwitter } from 'react-icons/fa';
 import { FaInstagram } from 'react-icons/fa6';
 import { IoIosRocket } from 'react-icons/io';
+import { IconLink } from '../base/IconLink';
 export const Header = () => {
   return (
     <header className="font-urbanist bg-black py-4 text-lg font-normal tracking-wider text-white uppercase">
@@ -15,9 +16,21 @@ export const Header = () => {
           <div className="flex items-center gap-12">
             <div>Nav Here</div>
             <div className="flex gap-4">
-              <FaFacebook />
-              <FaInstagram />
-              <FaTwitter />
+              <IconLink
+                Icon={FaFacebook}
+                url="https://www.facebook.com"
+                target="_blank"
+              />
+              <IconLink
+                Icon={FaInstagram}
+                url="https://www.instagram.com"
+                target="_blank"
+              />
+              <IconLink
+                Icon={FaTwitter}
+                url="https://www.x.com"
+                target="_blank"
+              />
             </div>
           </div>
         </div>
